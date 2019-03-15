ed24fb00d854da611abd6d37d8b112ba9050f9f6
YoungStream <suselanyang@gmail.com>
2019-03-15 21:37:26 +0800

    修改

--- app/extend/context.js
+++ app/extend/context.js
@@ -2,7 +2,7 @@
 module.exports = {
     // 登陆用户信息
     get loginUser() {
-        //1. 收银机websocket连接参数格式 pos-deviceId-merchantId-branchId
+        //1. websocket连接参数格式 server-deviceId-merchantId-branchId
         //2. h5手机网页端连接参数格式 wap-userId-merchantId-branchId-tableId
         const clientAuth = this.request.headers['clientauth'] || this.request.query.clientAuth || '';
         if (!this.cacheUser || clientAuth !== this.cacheUser.authPlain) {
